fb0ce0cf909ed7b3f9eb227af9f89e49d212edeb
abpriest <abpriest@gmail.com>
2016-04-17 12:38:49 -0400

    third time's the charm (I've lost count though)

--- horizontal.s
+++ horizontal.s
@@ -1,17 +1,16 @@
 @ horizontal.s
 
 
-.global horizontal:
+.global horizontal
 horizonal:
-    mov r2, r0  @pass sprite->attribute1 to a variable into the assembly function
-    cmp r1, #1  @if
-    beq .set   
-    and r3, r2, #0xefff
-    mov r2, r3
-    b .done
-.set:
-    orr r2, r2, #0x1000
-   
-.done:
-    mov r0, r2
-    mov pc, lr
+        mov r2, r0  @pass sprite->attribute1 to a variable into the assembly function
+        cmp r1, #1  @if
+        beq .set   
+        and r3, r2, #0xefff
+        mov r2, r3
+        b .done
+    .set:
+        orr r2, r2, #0x1000
+    .done:
+        mov r0, r2
+        mov pc, lr
