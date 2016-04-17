70843be8b668dd921765fdc025279941f6ac0efa
abpriest <abpriest@gmail.com>
2016-04-17 12:49:43 -0400

    I got a feelin'

--- horizontal.s
+++ horizontal.s
@@ -1,17 +1,15 @@
 @ horizontal.s
-
-
 .global horizontal
 horizonal:
-        mov r2, r0  @pass sprite->attribute1 to a variable into the assembly function
+        @r0 = sprite attribute
+        @r1 = boolean to flip
+        @r2 = the hex value to check against
         cmp r1, #1  @if
         beq .set   
-        mov r1, #0xefff
-        and r3, r2, r1
-        mov r2, r3
+        and r3, r0, r2
         b .done
     .set:
-        orr r2, r2, #0x1000
+        orr r3, r3, #0x1000
     .done:
-        mov r0, r2
+        mov r0, r3
         mov pc, lr
