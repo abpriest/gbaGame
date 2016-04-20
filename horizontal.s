9256885000192bb88030dffe66e6c5fbaee42f5c
abpriest <abpriest@gmail.com>
2016-04-20 13:20:33 -0400

    fixed the bug that made samus explode upon going left

--- horizontal.s
+++ horizontal.s
@@ -6,10 +6,9 @@ horizontal:
         @r2 = the hex value to check against
         cmp r1, #1  @if
         beq .set   
-        and r3, r0, r2
+        and r0, r0, r2
         b .done
     .set:
-        orr r3, r3, #0x1000
+        orr r0, r0, #0x1000
     .done:
-        mov r0, r3
         mov pc, lr
